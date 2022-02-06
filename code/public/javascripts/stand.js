4458fe93536fff74f18bfd90b75cedc65ee0c2ed
Miguel Morais <miguel1morais@gmail.com>
2022-02-06 15:38:40 +0000

    Html files almost done + Updated the populates of my database + Stand has a map

---
+++ code/public/javascripts/stand.js
@@ -0,0 +1,7 @@
+/*mapboxgl.accessToken = 'pk.eyJ1IjoibWlndWVsMW1vcmFpcyIsImEiOiJja3piZWswcmkwY2J2Mm9udDBocXFtNTZ6In0.aQaEqo-9OjYHo5Ynz9nbgA';
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/satellite-streets-v11', // style URL
+    center: [-8.628, 37.233], // starting position [lng, lat] Inserts are backwards inside batabase - lat, lng
+    zoom: 9 // starting zoom
+});*/
